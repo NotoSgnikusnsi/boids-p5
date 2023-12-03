73d0050b758add262dd2b07dd7ca3f0e154c0bc4
Noto <notosgnikusnsi@gmail.com>
2023-12-03 22:44:20 +0900

    feat: boidクラスの初期値を設定する

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+class Boid {
+  constructor() {
+    this.position = createVector(random(width), random(height));
+    this.velocity = p5.Vector.random2D(); // 大きさが1のランダムなベクトル
+    this.acceleration = createVector(0, 0);
+  }
+
+  // 結合
+  cohesion(boids) { }
+  // 分離
+  separation(boids) { }
+  // 整列
+  alignment(boids) { }
+
+
+}
+
 function setup() {
 }
 
