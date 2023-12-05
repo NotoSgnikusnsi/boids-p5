38690a6bdc4c22af4df03edce303d7c72f374232
notosgnikusnsi <notosgnikusnsi@gmail.com>
2023-12-05 09:18:48 +0900

    feat: 結合の処理を実装する

--- app.js
+++ app.js
@@ -6,7 +6,26 @@ class Boid {
   }
 
   // 結合
-  cohesion(boids) { }
+  cohesion(boids) {
+    // 1. 結合したい仲間の距離を決める
+    const radius = 100;
+    const sum = createVector(0, 0);
+    const count = 0;
+    // 2. 仲間の距離を計算する
+    for (let boid of boids) {
+      const distance = this.position.dist(boid.position);
+      if (distance < radius) {
+        sum.add(boid.position);
+        count++;
+      }
+    }
+    // 3. 仲間の平均位置に向かうベクトルを計算する
+    sum.div(count);
+    sum.sub(this.position);
+    sum.setMag(0.1);
+    // 4. 速度を更新する
+    this.acceleration.add(sum);
+  }
   // 分離
   separation(boids) { }
   // 整列
