9cef7a7cb5c90de978c2faaed39466573c78e215
notosgnikusnsi <notosgnikusnsi@gmail.com>
2023-12-05 09:30:48 +0900

    feat: 分離の処理を実装する

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ class Boid {
     const radius = 100;
     const sum = createVector(0, 0);
     const count = 0;
-    // 2. 仲間の距離を計算する
+    // 2. 仲間の合計距離を計算する
     for (let boid of boids) {
       const distance = this.position.dist(boid.position);
       if (distance < radius) {
@@ -26,8 +26,29 @@ class Boid {
     // 4. 速度を更新する
     this.acceleration.add(sum);
   }
+
   // 分離
-  separation(boids) { }
+  separation(boids) {
+    // 1. 分離したい仲間の距離を決める
+    const radius = 50;
+    const sum = createVector(0, 0);
+    const count = 0;
+    // 2. 仲間の合計距離を計算する
+    for (let boid of boids) {
+      const distance = this.position.dist(boid.position);
+      if (distance < radius) {
+        sum.add(boid.position);
+        count++;
+      }
+    }
+    // 3. 仲間から離れるベクトルを計算する
+    sum.div(count);
+    sum.sub(this.position);
+    sum.setMag(0.1);
+    // 4. 速度を更新する
+    this.acceleration.sub(sum);
+  }
+
   // 整列
   alignment(boids) { }
 
